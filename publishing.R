b0a21d1ca289eef75f7371595aa2b907e59419e6
Natalie Namesna <hanusova.nat@gmail.com>
2026-01-30 10:19:24 +0100

    new script

---
+++ publishing.R
@@ -0,0 +1,18 @@
+#PUBLISHING
+usethis::use_git()
+usethis::use_github()
+
+# MIT
+usethis::use_mit_license("Natalie Namesna")
+
+# CC0
+usethis::use_cc0_license()
+
+# CC-BY
+usethis::use_ccby_license()
+
+# code that you don't want to share with others
+usethis::use_proprietary_license("Natalie Namesna")
+
+#code of condact
+usethis::use_code_of_conduct("hanusova.nat@gmail.com")
